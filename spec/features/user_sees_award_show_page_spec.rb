6a341ff9e9542ef3dfcc16005e395fcdd85d211c
JamisonOrdway <jamisonordway@gmail.com>
2018-06-06 10:45:40 -0600

    All movies with this award implemented

--- spec/features/user_sees_award_show_page_spec.rb
+++ spec/features/user_sees_award_show_page_spec.rb
@@ -3,15 +3,20 @@ require 'rails_helper'
 describe 'User visits award show' do
   context 'as visitor' do
     it 'should show all the movies which received this award and the year it was received' do
-      award_1 = Award.create(name: 'Most Realistic Gore')
-      movie_1 = Movie.create(title: 'SAW III', description: 'because 2 wasnt enough')
-      movie_2 = Movie.create(title: 'House of Wax', description: 'the guy from supernatural melts its pretty cool')
-      movie_award_1 = award_1.movie_awards.create(award_id: award_1.id, movie_id: movie_1.id, year: '2011')
-      movie_award_2 = award_1.movie_awards.create(award_id: award_1.id, movie_id: movie_2.id, year: '2005')
+      director = Director.create(name: 'some guy')
 
+      award_1 = Award.create!(name: 'Most Realistic Gore')
+
+      movie_1 = director.movies.create!(title: 'SAW III', description: 'because 2 wasnt enough')
+      movie_2 = director.movies.create!(title: 'House of Wax', description: 'the guy from supernatural melts its pretty cool')
+
+      movie_award_1 = movie_1.movie_awards.create!(award_id: award_1.id, movie_id: movie_1.id, year: '2009')
+      movie_award_2 = movie_2.movie_awards.create!(award_id: award_1.id, movie_id: movie_2.id, year: '2010')
+
+      
       visit award_path(award_1)
-      #save_and_open_page
-  
+      save_and_open_page
+
       expect(page).to have_content(movie_1.title)
       expect(page).to have_content(movie_2.title)
       expect(page).to have_content(movie_award_1.year)
