6c2803aad47f943165f33349154106e142eaefa2
JamisonOrdway <jamisonordway@gmail.com>
2018-06-06 10:11:34 -0600

    Movie awards on movie index WIP

--- spec/features/user_sees_all_movies_spec.rb
+++ spec/features/user_sees_all_movies_spec.rb
@@ -6,19 +6,20 @@ describe 'user_index' do
     movie_1 = director.movies.create(title: "Mean Girls", description: "High school girls are mean.")
     movie_2 = Movie.create!(title: "Boss Baby", description: "A baby is a boss.", director_id: director.id)
     movie_3 = Movie.create(title: "Catch Me If You Can", description: "Boy becomes expert in check fraud", director: director)
-    award_1 = movie_3.awards.create(name: 'Best Kids Film')
-    award_2 = movie_3.awards.create(name: 'Least Likely to be Enjoyed by Adults')
+    award_1 = Award.create(name: 'Best')
+    aware_2 = Award.create(name: 'Worst')
+    movie_award_1 = movie_1.movie_awards.create(award_id: award_1.id, movie_id: movie_1.id, year: '2009')
 
     visit '/movies'
-    binding.pry
+    save_and_open_page
+    #binding.pry
     expect(page).to have_content(movie_1.title)
     expect(page).to have_content(movie_1.description)
     expect(page).to have_content(movie_2.title)
     expect(page).to have_content(movie_2.description)
     expect(page).to have_content(movie_3.title)
     expect(page).to have_content(movie_3.description)
-    expect(page).to have_content("Awards for this Movie: ")
-    expect(page).to have_content('Least Likely to be Enjoyed by Adults')
-    expect(page).to have_content('Best Kids Film')
+    expect(page).to have_content(award_1.name)
+    expect(page).to have_content(movie_award_1.year)
   end
 end
